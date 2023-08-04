e9cbc88663f09399acb91255b80c7962e5da7c89
Jojhan Silva <silvajojhan@gmail.com>
2023-08-04 15:39:18 -0500

    added project body

--- src/components/project.js
+++ src/components/project.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React , { useState } from 'react';
+import AboutMe from './pages/me';
+import Contact from './pages/contact';
+import Resume from './pages/resume';
+import Portfolio from './pages/portfolio'
+import Nav from './navigation';
 
 function Project() {
-    return(
-        <header>
-            <h2>Test</h2>
-        </header>
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+  
+    const renderPage = () => {
+      if (currentPage === 'Portfolio') {
+        return <Portfolio />;
+      }
+      if (currentPage === 'Contact') {
+        return <Contact />;
+      }
+      if (currentPage === 'Resume') {
+        return <Resume />;
+      }
+      return <AboutMe />;
+    };
+  
+    const handlePageChange = (page) => setCurrentPage(page);
+  
+    return (
+      <div>
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+      </div>
     );
+
 }
 
 export default Project;
