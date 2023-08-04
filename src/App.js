4e45c856f49fef92a1e307e862a8bc327bf13408
Jojhan Silva <silvajojhan@gmail.com>
2023-08-04 14:55:16 -0500

    added navlinks

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Header from './components/header';
 import Project from './components/project';
 import Footer from './components/footer';
+import Nav from './components/navigation';
 
 const App = () => {
   return(
       <div>
           <Header/>
+          <Nav />
           <Project />
           <Footer />
       </div>
