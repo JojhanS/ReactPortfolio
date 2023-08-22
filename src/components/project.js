701a88ed0263396c62cad85aac0c4015e95ce90f
Jojhan Silva <silvajojhan@gmail.com>
2023-08-21 20:29:26 -0500

    added routes to portfolio, nav links work

--- src/components/project.js
+++ src/components/project.js
@@ -1,36 +1,26 @@
-import React , { useState } from 'react';
-// import { useRoutes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AboutMe from './pages/me';
 import Contact from './pages/contact';
 import Resume from './pages/resume';
 import Portfolio from './pages/portfolio'
 import Nav from './navigation';
 
-function Project() {
-    const [currentPage, setCurrentPage] = useState('AboutMe');
-  
-    const renderPage = () => {
-      if (currentPage === 'Portfolio') {
-        return <Portfolio />;
-      }
-      if (currentPage === 'Contact') {
-        return <Contact />;
-      }
-      if (currentPage === 'Resume') {
-        return <Resume />;
-      }
-      return <AboutMe />;
-    };
-  
-    const handlePageChange = (page) => setCurrentPage(page);
-  
-    return (
-      <div>
-        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-      </div>
-    );
 
+function Project() {
+  return (
+      <Router>
+          <div>
+              <Nav />
+              <Routes>
+                  <Route path="/portfolio" element={<Portfolio />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/resume" element={<Resume />} />
+                  <Route path="/" element={<AboutMe />} />
+              </Routes>
+          </div>
+      </Router>
+  );
 }
 
 export default Project;
