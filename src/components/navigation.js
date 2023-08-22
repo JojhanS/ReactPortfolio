1574fe65b8c988568c7a724fccc644c0116222f8
Jojhan Silva <silvajojhan@gmail.com>
2023-08-21 20:22:36 -0500

    when route is empty load aboutme section

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Nav() {
     return(
         <ul>
-            <li><Link to="/about">About Me</Link></li>
+            <li><Link to="/">About Me</Link></li>
             <li><Link to="/portfolio">Portfolio</Link></li>
             <li><Link to="/contact">Contact</Link></li>
             <li><Link to="/resume">Resume</Link></li>
