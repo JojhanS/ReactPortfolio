009d6c8c2be23db261c399a7495fccb715df07cb
Jojhan Silva <silvajojhan@gmail.com>
2023-08-21 20:36:40 -0500

    added "portfolio"

---
+++ src/components/pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Portfolio() {
+    return(
+        <div>
+            <p>
+                Portfolio Goes Here!
+            </p>
+        </div>
+    );
+}
+
+export default Portfolio;
